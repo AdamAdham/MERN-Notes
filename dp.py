dee97ed62daad2e372037e024f3db85d6e7e6af1
AdamAdham <adamfayek@yahoo.com>
2024-09-18 21:46:51 +0300

    adding the x

--- dp.py
+++ dp.py
@@ -3,6 +3,7 @@ def max_profit(grid):
     grid = np.array(grid)
     profit = np.zeros(grid.shape)
     path = np.zeros(grid.shape)
+    x = 1 + 2
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if(i==0 and j==0):
@@ -18,6 +19,8 @@ def max_profit(grid):
                 val,cord = get_max(grid[i-1][j],(i-1,j),grid[i][j-1],(i,j-1))
                 profit[i][j] = val + grid[i][j]
                 path[i][j] = path[i-1][j] + [cord]
+    return x
+
 
 def get_max(a,a_cord,b,b_cord):
     if(a>b):
